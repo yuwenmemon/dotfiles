f63c528f097677882007d9795634d83bf60220db
Carlos Alvarez <carlos@carlos.local>
2014-02-09 19:41:38 -0800

    Improved makesymlinks

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                      # dotfiles directory
 olddir=~/dotfiles_old               # old dotfiles backup directory
-files="bashrc vimrc vim gitconfig"  # list of files/folders to symlink in homedir
+files="bashrc vimrc vim gitconfig bash_profile aliases"  # list of files/folders to symlink in homedir
 
 ##########
 
@@ -21,11 +21,26 @@ echo "done"
 echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
+printf "\n\n"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+printf '\e[1;34m%-6s\e[m' "Removing existing backup files"
+printf "\n"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
+    rm $olddir/$file
+done
+printf "\n\n"
+
+printf '\e[1;34m%-6s\e[m' "Moving any existing dotfiles from $dir to $olddir"
+printf "\n"
+for file in $files; do
+    mv ~/$file $olddir/$file
+done
+printf "\n\n"
+
+printf '\e[1;34m%-6s\e[m' "Creating symlink to $file in home directory"
+printf "\n"
+for file in $files; do
     ln -s $dir/$file ~/.$file
 done
+printf "\n\n"
